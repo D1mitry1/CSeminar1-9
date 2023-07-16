15c471b6769dc5888dd8731b96b9dfbfcad96796
Dmitrey Lokosov <borisovich84-ldb@mail.ru>
2023-07-16 20:12:04 +0400

    Added Task09

--- Task09/Program.cs
+++ Task09/Program.cs
@@ -7,8 +7,19 @@
 
 int number = new Random().Next(10, 100); // 99+1 = 100
 Console.WriteLine($"Random number range 10 - 99 -> {number}");
-int firstDigit = number / 10; // 78 / 10 = 7
-int secondDigit = number % 10; // 78 = 70 + 8
-if (firstDigit > secondDigit) Console.WriteLine($"Maximum digit of the number -> {firstDigit}");
-else Console.WriteLine($"Maximum digit of the number -> {secondDigit}");
-                                                                              
+// int firstDigit = number / 10; // 78 / 10 = 7
+// int secondDigit = number % 10; // 78 = 70 + 8
+// if (firstDigit > secondDigit) Console.WriteLine($"Maximum digit of the number -> {firstDigit}");
+// else Console.WriteLine($"Maximum digit of the number -> {secondDigit}");
+
+int maxDigit = MaxDigit(number);
+Console.WriteLine($"Maximum digit of the number -> {maxDigit}");
+
+int MaxDigit(int num)
+{
+    int firstDigit = number / 10;
+    int secondDigit = number % 10;
+    // if (firstDigit > secondDigit) return firstDigit;
+    // return secondDigit;
+    return firstDigit > secondDigit ? firstDigit : secondDigit;
+}
