429ead0a91b7019f0f827184ecf92263f849e405
Dmitrey Lokosov <borisovich84-ldb@mail.ru>
2023-07-17 19:30:46 +0400

    Added Task14

---
+++ Task14/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает 
+// на вход число и проверяет, кратно ли 
+// оно одновременно 7 и 23.
+// 14 -> нет
+// 46 -> нет
+// 161 -> да 
+
+Console.WriteLine("Enter number");
+int number = Convert.ToInt32(Console.ReadLine());
+
+bool num = IsMultiple(number);
+Console.WriteLine(num ? "Yes" : "No");
+
+bool IsMultiple(int num)
+{
+    return num % 7 == 0 && num % 23 == 0;
+}
---
+++ Task16/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает
+// 1. на вход два числа и проверяет, является ли
+// 2. одно число квадратом другого.
+// Например:
+// 5, 25 -> да
+// -4, 16 -> да
+// 25, 5 -> да
+// 8, 9 -> нет
+
+Console.WriteLine("Enter first number");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter second number");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+bool res = IsSquareTwoNumbers(number1, number2);
+Console.WriteLine(res ? "Yes" : "No");
+
+bool IsSquareTwoNumbers(int num1, int num2)
+{
+    return num1 == num2 * num2 || num2 == num1 * num1;
+}
