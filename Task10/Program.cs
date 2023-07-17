0a8f4c8d554617bb1d9b59d2971fb3470fc14998
Dmitrey Lokosov <borisovich84-ldb@mail.ru>
2023-07-17 23:32:22 +0400

    Added Task10

---
+++ Task10/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая принимает
+// на вход трехзначное число и на
+// выходе показывает вторую цифру этого числа
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
+
+int number = Prompt("Enter three-digit number > ");
+if (number < 100 || number >= 1000)
+{
+    Console.WriteLine("You did not enter a three-digit number, please re-enter");
+    return;
+}
+Console.WriteLine($"Entered number `{number}`");
+int secondRank = number / 10 % 10;
+Console.WriteLine($"Second digit `{secondRank}`");
