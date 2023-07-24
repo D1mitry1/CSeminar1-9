8073ab816d9876769e1eaf1ea9153d376890a49e
Dmitrey Lokosov <borisovich84-ldb@mail.ru>
2023-07-24 12:51:10 +0400

    Added Task19

---
+++ Task19/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая принимает на
+// вход пятизначное число и проверяет, 
+// является ли оно палиндромом.
+// Выполнить с помощью числовых операций (целочисленное деление, остаток от деления).
+// 14212 -> нет
+// 23432 -> да
+// 12821 -> да
+
+int num, r, sum = 0, t;
+Console.Write("Enter a five-digit number: ");
+num = Convert.ToInt32(Console.ReadLine());
+if (num > 9999 && num < 100000)
+{
+    for (t = num; num != 0; num = num / 10)
+    {
+        r = num % 10;
+        sum = sum * 10 + r;
+    }
+    if (t == sum)
+        Console.Write($"{t} is a palindrome, ");
+    else
+        Console.Write($"{t} is not a palindrome, ");
+}
+else
+    Console.Write("Enter a five-digit number! ");
+
---
+++ Task23/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает
+// на вход число (N) и выдает таблицу
+// кубов чисел от 1 до N
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125 
+
+Console.WriteLine("Enter a positive integer");
+int number = Convert.ToInt32(Console.ReadLine());
+TableCube(number);
+
+void TableCube(int num)
+{
+    int count = 1;
+    while (count <= num)
+    {
+        Console.WriteLine($"{count}  {count * count * count}");
+        count++;
+    }
+}
